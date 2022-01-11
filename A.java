5525eaf5955bfdd65d3159aa337d5a920da1ebf7
Anand Kumar <anandkumarramisetti@gmail.com>
2022-01-11 19:22:02 +0530

    anand

---
+++ A.java
@@ -0,0 +1,18 @@
+package inheritance.single;
+
+public class A {
+	
+	public void add() {
+		int a= 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println("the sum is : "+c);
+	}
+	void name() {
+		System.out.println("kumar");
+	}
+	void data(int n,String a) {
+		System.out.println("integer is :"+n);
+		System.out.println("String is :"+a);
+	}
+}
